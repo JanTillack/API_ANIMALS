b68ce9782ab487e47f7bd7354f533f4fa6fc793c
Jan Tillack <ford_gt90@wp.pl>
2016-08-06 16:41:22 +0200

    Added editing of info

--- app.js
+++ app.js
@@ -1,77 +1,112 @@
 var main = function() {
-    var apiUrl = 'http://localhost:3000/api';
+  var apiUrl = 'http://localhost:3000/api';
+  getAnimals();
+  function getAnimals() {
+   $.ajax({
+    url: apiUrl + '/animals',
+    method: 'GET'
+  })
+   .then(function(data) {
+    printTable(data);
+  });
+ }
+ function getOneAnimal (id){
+  $.ajax({
+    url: apiUrl + '/animals/' + id,
+    method: 'GET',
+  }).then(function(data) {
+    showDetails(data);
+  });
+}
+function addAnimal(requestBody) {
+  $.ajax({
+    url: apiUrl + '/animals',
+    method: 'POST',
+    data: requestBody
+  }).then(function() {
     getAnimals();
-    function getAnimals() {
-        $.ajax({
-            url: apiUrl + '/animals',
-            method: 'GET',
-        }).then(function(data) {
-            printTable(data);
-        });
-    }
-    function getOneAnimal(id) {
-        $.ajax({
-            url: apiUrl + '/animals/' + id,
-            method: 'GET',
-        }).then(function(data) {
-            showDetails(data);
-        });
-    }
-    function addAnimal(requestBody) {
-        $.ajax({
-            url: apiUrl + '/animals' ,
-            method: 'POST',
-            data: requestBody
-        }).then(function() {
-            getAnimals();
-        });
-    }
-
-
-    function deleteAnimal(id) {
-        $.ajax({
-            url: apiUrl + '/animals/' +  id,
-            method: 'DELETE',
-        }).then(function(data) {
-            getAnimals();
-        });
-    }
-
-    function printTable(animals) {
-        $('#animals').empty();
-        animals.forEach(function(animal) {
-            var showDetailsButton = $('<button>SHOW DETAILS</button>').click(function(){
-                getOneAnimal(animal._id);
-            });
-            var deleteButton = $('<button>DELETE ANIMAL</button>').click(function(){
-                deleteAnimal(animal._id);
-                });
-
-            
-            $('#animals').append('<tr><td class="name">' + animal.name + 
-                '</td><td class="species">' + animal.species + '</td>' +
-                '</tr>').append(showDetailsButton).append(deleteButton);
-        });
-    }
-    
-    function showDetails(animal) {
-        $('#details').text('IMIĘ: ' + animal.name + ' WŁAŚCICIEL: ' + animal.owner + ' WIEK: ' +
-            animal.age + ' GATUNEK: ' + animal.species);
-    }
-
-    
-    
-    function getInputs () {
-        var animal = {
-            name: $('#name').val(),
-            age: $('#age').val(),
-            species: $('#species').val(),
-            owner: $('#owner').val()
-        }
-        addAnimal(animal);
-    }
-    $('#add_animal').click(function(){
-        getInputs();
+  });
+}
+function updateAnimal(requestBody) {
+  $.ajax({
+    url: apiUrl + '/animals/' + requestBody._id,
+    method: 'PUT',
+    data: requestBody
+  }).then(function() {
+    getAnimals();
+  });
+}
+function deleteAnimal(id) {
+  $.ajax({
+    url:apiUrl + '/animals/' + id,
+    method: 'DELETE',
+  }).then(function(data){
+   getAnimals();
+ })
+  
+}
+function printTable(data) {
+  $('#animals').empty();
+  data.forEach(function(animal,i) {
+    var showDeleteButton = $('<td><button>DELETE</button></td>').click(function() {
+      deleteAnimal(animal._id);
     })
+    var showDetailsButton = $('<td><button>SHOW DETAILS</button></td>').click(function() {
+      getOneAnimal(animal._id);
+    })
+    var tableRows = $('#animals').append('<tr></tr>').children();
+    
+    var row = tableRows[i];
+    
+    $(row).append('<td>' + (i+1) + 
+      '</td><td class="currency">' + animal.name + 
+      '</td>').append(showDetailsButton).append(showDeleteButton);
+  });
+}
+function showDetails(animal) {
+  var showInputsButton = $('<button>EDIT</button>').click(function() {
+    showInputs(animal);
+  });
+
+  $('#details').empty();
+  $('#details').append('<p>IMIĘ:' + animal.name + '</p>');
+  $('#details').append('<p>WIEK: ' + animal.age + '</p>');
+  $('#details').append('<p>WŁAŚCICIEL: ' + animal.owner + '</p>');
+  $('#details').append('<p>GATUNEK: ' + animal.species + '</p>');
+  $('#details').append(showInputsButton);
+  
+}
+function showInputs(animal){
+  var updateButton = $('<button>UPDATE</button>').click(function(){
+    animal.name = $('#update_name').val();
+    animal.age = $('#update_age').val();
+    animal.species = $('#update_species').val();
+    animal.owner = $('#update_owner').val();
+    
+    updateAnimal(animal);
+  });
+  $('#details').empty();
+  $('#details').append('<p>IMIĘ: <input type="text" id="update_name"></p>');
+  $('#details').append('<p>WIEK: <input type="number" id="update_age"></p>');
+  $('#details').append('<p>WŁAŚCICIEL: <input type="text" id="update_owner"></p>');
+  $('#details').append('<p>GATUNEK: <input type="text" id="update_species"></p>');
+  $('#details').append(updateButton);
+  $('#update_name').val(animal.name);
+  $('#update_age').val(animal.age);
+  $('#update_owner').val(animal.owner);
+  $('#update_species').val(animal.species);
+}
+function getInputs() {
+  var animal = { 
+    name: $('#name').val(),
+    age: $('#age').val(),
+    species: $('#species').val(),
+    owner: $('#owner').val()
+  }
+  addAnimal(animal);
+}
+$('#add_animal').click(function() {
+  getInputs();
+})
 }
 $(document).ready(main);
