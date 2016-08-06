8a8d2e73052dff8ef740c1b85dc20e2859cc14c6
Jan Tillack <ford_gt90@wp.pl>
2016-08-06 14:54:31 +0200

    Added Button/Fixed table

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
 var main = function() {
     var apiUrl = 'http://localhost:3000/api';
-
     getAnimals();
-
     function getAnimals() {
         $.ajax({
-          url: apiUrl + '/animals',
-          method: 'GET',
-      }).then(function(data) {
-        printTable(data);
-        console.log(data);
-    });
-
-  }
- 
-  function addAnimal(requestBody){
-    $('#animals').empty();
-    $.ajax({
-          url: apiUrl + '/animals',
-          method: 'POST',
-          data:requestBody
-      }).then(function(data) {
-        getAnimals();
-    });
-  }
-
-  function printTable(animals) {
-
-    for(var i = 0; i<animals.length; i++){
-        $('#animals').append('<tr><td>' + (i+1) + '</td><td>' + 
-            animals[i].name + '</td><td>' + animals[i].species +
-            '</td><td>' + animals[i].age + '</td><td>' + animals[i].owner + '</td></tr>')
+            url: apiUrl + '/animals',
+            method: 'GET',
+        }).then(function(data) {
+            printTable(data);
+        });
     }
-}
-
-function getInputs() {
-    var animal = {
-        name: $('#name').val(),
-        age: $('#age').val(),
-        species: $('#species').val(),
-        owner: $('#owner').val()
+    function getOneAnimal(id) {
+        $.ajax({
+            url: apiUrl + '/animals/' + id,
+            method: 'GET',
+        }).then(function(data) {
+            showDetails(data);
+        });
     }
-    addAnimal(animal);
+    function addAnimal(requestBody) {
+        $.ajax({
+            url: apiUrl + '/animals',
+            method: 'POST',
+            data: requestBody
+        }).then(function() {
+            getAnimals();
+        });
+    }
+    function printTable(animals) {
+        $('#animals').empty();
+        animals.forEach(function(animal) {
+            var showDetailsButton = $('<button>SHOW DETAILS</button>').click(function(){
+                getOneAnimal(animal._id);
+            });
+            $('#animals').append('<tr><td class="name">' + animal.name + 
+                '</td><td class="species">' + animal.species + '</td>' +
+                '</tr>').append(showDetailsButton);
+        });
+    }
+    function showDetails(animal) {
+        $('#details').text('IMIĘ: ' + animal.name + ' WŁAŚCICIEL: ' + animal.owner + ' WIEK: ' +
+            animal.age + ' GATUNEK: ' + animal.species);
+    }
+    function getInputs () {
+        var animal = {
+            name: $('#name').val(),
+            age: $('#age').val(),
+            species: $('#species').val(),
+            owner: $('#owner').val()
+        }
+        addAnimal(animal);
+    }
+    $('#add_animal').click(function(){
+        getInputs();
+    })
 }
-
-$('#add_animal').click(function(){
-    getInputs();
-})
-}
-
 $(document).ready(main);
-
-
